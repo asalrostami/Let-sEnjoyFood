12cc7ba95623342b806a61da9d7d361b4ecbcc4a
asal <asal.rostami1984@gmail.com>
2017-11-20 12:54:01 -0500

    Set login page

--- app/src/main/java/com/example/asal/letsenjoyfood/RegistrationActivity.java
+++ app/src/main/java/com/example/asal/letsenjoyfood/RegistrationActivity.java
@@ -2,6 +2,7 @@ package com.example.asal.letsenjoyfood;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+    }
+
+    public void register_ClickR(View view) {
     }
 }
